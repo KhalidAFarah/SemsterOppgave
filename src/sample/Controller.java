733ec2b907c6bf9d9365eb864c18265ede39ca87
Lars <abbeilarsh123@gmail.com>
2020-04-01 20:57:20 +0200

    Register_GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,13 @@
 package sample;
 
 import Brukere.Bruker;
+import Brukere.Standardbruker;
+import Brukere.Superbruker;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -28,6 +31,10 @@ public class Controller {
     private CheckBox chxStandarbruker;
 
     @FXML
+    private Label txtError;
+
+
+    @FXML
     void onClick_btn_Avbryt(ActionEvent event) {
         Platform.exit();
 
@@ -35,6 +42,21 @@ public class Controller {
 
     @FXML
     void onClick_btn_Register(ActionEvent event) {
+        Bruker b = new Bruker();
+        b.setBrukernavn(txtBrukernavn.getText());
+        b.setPassord(txtPassord.getText());
+       // b.setTelefonnummer(txtTelefonnummer.getText());
+        b.setEmail(txtEmail.getText());
+
+        Bruker A;
+        if(chxAdmin.isSelected() && !chxStandarbruker.isSelected()){
+            A = new Superbruker(b);
+        }else if(chxStandarbruker.isSelected() && !chxAdmin.isSelected()){
+            A = new Standardbruker(b);
+        }else{
+            txtError.setText("");
+        }
+
 
 
 
