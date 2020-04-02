2430bba4e641d113e4d7302c17c1053260f8418e
Kisan Srirangan <kisan@live.no>
2020-04-02 14:35:07 +0200

    små endringer på sample

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Registrer ny bruker");
         primaryStage.setScene(new Scene(root, 650, 450));
         primaryStage.show();
     }
