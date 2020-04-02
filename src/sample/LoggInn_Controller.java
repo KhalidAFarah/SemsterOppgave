322ae60b0dfa5c6526511463063ff31b166a2186
Lars <abbeilarsh123@gmail.com>
2020-04-02 15:25:55 +0200

    Register_GUI

---
+++ src/sample/LoggInn_Controller.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoggInn_Controller {
+
+    @FXML
+    private TextField txtBrukernavn;
+
+    @FXML
+    private TextField txtPassord;
+
+    @FXML
+    void onClick_btn_LoggInn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClick_btn_RegistrerNyBruker(ActionEvent event) {
+
+    }
+
+}
