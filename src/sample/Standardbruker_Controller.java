3b4089b95f83fd633ad7b3d42bb022dcee44666a
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2020-04-06 16:57:23 +0200

    dynamisk lager liste av type komponenter

--- src/sample/Standardbruker_Controller.java
+++ src/sample/Standardbruker_Controller.java
@@ -1,20 +1,27 @@
 package sample;
 
+import filbehandling.FiledataJOBJ;
+import filbehandling.FiledataTxt;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.SubScene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
+import komponenter.Komponenter;
+import static javax.swing.JOptionPane.*;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Standardbruker_Controller {
 
@@ -32,6 +39,21 @@ public class Standardbruker_Controller {
 
     @FXML
     private AnchorPane pane;
+
+    private Komponenter komponenter = new Komponenter();
+
+    private int userNr;
+
+    public void loadKomponenter(){
+        FiledataJOBJ filedata = new FiledataJOBJ();
+        Path path = Paths.get("src/filbehandling/LagredeKomponenter");
+
+        try{
+            filedata.load(komponenter, path);
+        }catch (Exception e){
+            showMessageDialog(null, e.getMessage());
+        }
+    }
 
     @FXML
     void On_Click_BtnTilbake(ActionEvent event) throws IOException {
@@ -83,6 +105,33 @@ public class Standardbruker_Controller {
         la.setLayoutY(175.0);
 
         pane.getChildren().add(la);
+
+        int y = 50;
+        loadKomponenter();
+        for(int i = 0; i < komponenter.getMainArray().size(); i++) { // lag en komponent array senere
+            //ImageView img = new ImageView();
+
+            if (komponenter.getMainArray().get(i).getType().equals("Prosessor")) {
+
+                Label label = new Label(komponenter.getMainArray().get(i).getNavn());
+                label.setLayoutY(y);
+                Button btn = new Button("Velg");
+                btn.setLayoutY(y + 25);
+                y += 50;
+
+                userNr = i;
+
+                btn.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        System.out.println("denne varen er valgt" + komponenter.getMainArray().get(userNr).getNavn());
+                    }
+                });
+
+                pane.getChildren().add(label);
+                pane.getChildren().add(btn);
+            }
+        }
 
     }
 
