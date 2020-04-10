4c5015b6df2d8423ec9413ffd513e0d53d08c540
Lars <abbeilarsh123@gmail.com>
2020-04-10 13:26:44 +0200

    Logg_inn.fxml endringer

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Superbruker.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoggInn.fxml"));
         primaryStage.setTitle("TechMet");
       //primaryStage.setScene(new Scene(root, 400, 400)); //details for LoggInn.FXML
        primaryStage.setScene(new Scene(root, 602.4, 400)); //test for andre fxml filer
