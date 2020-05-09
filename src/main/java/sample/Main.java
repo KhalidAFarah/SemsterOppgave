cd0581f7eead48ce9afee7c60e485bca82a24bfd
Kisan Srirangan <56064454+kisansrirangan@users.noreply.github.com>
2020-05-09 16:30:19 +0200

    standardbruker mellomside

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/LoggInn.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/Standardbruker_mellomside.fxml"));
             primaryStage.setTitle("TechMet");
             primaryStage.setScene(new Scene(root, 420, 450)); //details for LoggInn.FXML
             //primaryStage.setScene(new Scene(root, 602.4, 400)); //test for andre fxml filer
