a07e399e880501af15285ea006be68534e5cd89e
Kisan Srirangan <56064454+kisansrirangan@users.noreply.github.com>
2020-05-10 20:29:02 +0200

    hmm

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,10 +13,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
+            //BorderPane root = new BorderPane();
+
             Parent root = FXMLLoader.load(getClass().getResource("LoggInn.fxml"));
             primaryStage.setTitle("TechMet");
-            primaryStage.setScene(new Scene(root, 420, 450)); //details for LoggInn.FXML
+            Scene scene = new Scene(root, 420, 450);
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+            //primaryStage.setScene(new Scene(root, 420, 450)); //details for LoggInn.FXML
             //primaryStage.setScene(new Scene(root, 602.4, 400)); //test for andre fxml filer
+            primaryStage.setScene(scene);
             primaryStage.show();
 
         } catch (IOException e) {
